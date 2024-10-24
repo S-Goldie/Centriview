d878dc871c9e915652523ddf3bd3a1b7c233f438
Stuart Goldie <62567558+S-Goldie@users.noreply.github.com>
2024-10-24 12:42:14 +0200

    Update Pg1.py
    Reduced back-end by importing constants and material property functions from properties.py.

--- Centriview/pages/1_Fraction Population Prediction.py
+++ Centriview/pages/1_Fraction Population Prediction.py
@@ -14,81 +14,27 @@ import numpy as np
 import mpld3
 
 
-##_BACKEND DATA AND FUNCTIONS_##
-#Storage of parameters for calculations
-#"Keys" : ['Density / kg m-3', 'Layer Thickness / m', 'k1', 'k2']
-MATERIALS = {
-    "Graphene" :[2260, 3.35E-10, 2.5, 280],
-    "WS2" :     [7500, 6.18E-10, 1.75, 46],
-    "MoS2" :    [5060, 6.15E-10, 1.8, 45],
-    "NiPS3" :   [3180, 6.6E-10, 2.0, 45],
-    "MnPS3" :   [2900, 6.8E-10, 2.0, 45],
-    "GaS" :     [3860, 7.5E-10, 2.0, 45],
-    "BN" :      [2100, 3.4E-10, 2.0, 45],
-    "NiOH2" :   [4100, 4.6E-10, 2.0, 45],
-    "CuOH2" :   [3370, 5E-10, 2.0, 45],
-    "RuCl3" :   [3260, 6E-10, 2.0, 45],
-    "CrTe3" :   [4700, 1.1E-9, 2.0, 45]
-    }
+##_BACKEND DATA_##
+#Import backend data from properties file#
+import os
+import sys
 
-#"Keys" : ['density_m', 'density_c', 'viscosity_A', 'viscosity_Ea']
-#Constants produce values in common units, not SI. Conversion done in the function
-SOLVENTS = {
-    "IPA" :     [-8.97149E-4, 0.80402, 3.05818E-4, 21846.037],
-    "THF" :     [-0.00108, 0.90959, 0.0445145, 5845.749],
-    "NMP" :     [-8.81678E-4, 1.04999, 0.015821, 11553.57],
-    "DLG (Cyrene™)" : [-9.73846E-4, 1.27159, 9.089E-04, 23410],
-    "Toluene" : [-9.47951E-4, 0.88605, 0.0185636, 8438.939],
-    "DMF" :     [-9.62251E-4, 0.96862, 0.021271, 9047.422],
-    "Ethanol" : [-9.11871E-4, 0.80885, 0.003494, 14223.19],
-    }
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
 
-solvent_list = ['Water'] + list(SOLVENTS.keys()) + ['IPA:Water Mixture','Other']
-materials_list = list(MATERIALS.keys()) + ['Other']
+sys.path.append(parent_dir)
+import properties as prop
 
-#Constants for two phase mixtures, fitted using a crude two-dimensional polynomial
-IPAWaterDensity = [0.999581, -0.0867712, -0.000109338, -0.171483, -0.00165038, -1.24842e-06, 0.0592866, 0.00105765, -2.63718e-07, -1.86795e-08]
-IPAWaterViscosity = [1.78798, 14.9948, -0.0677034, -13.7734, -0.283823, 0.00133142, 1.61501, 0.194239, 0.000875951, -9.46676e-06]
+pS = 1595           #surfactant layer density, from Hirsham using anhydrous cholate on surface
+d0 = 4.25E-10       #surfactant layer thickness, from Hirsham
 
-def solvent_viscosity(temp, solvent):
-    """Returns the solvent viscosity in SI units given correct constants"""
-    R = 8.314
-    A = SOLVENTS[solvent][2]
-    E_a = SOLVENTS[solvent][3]
-    Kelvin = 273.15 + temp
-    centipoise = A*np.exp(E_a/(R*Kelvin))
-    return(centipoise/1000)
+solvent_list = ['Water'] + list(prop.SOLVENTS.keys()) + ['IPA:Water Mixture','Other']
+materials_list = list(prop.MATERIALS.keys()) + ['Other']
 
-def solvent_density(temp, solvent):
-    """Returns the solvent density in g cm-3"""
-    m = SOLVENTS[solvent][0]
-    c = SOLVENTS[solvent][1]
-    return((m * temp + c)*1000)
+solvent_list = ['Water'] + list(prop.SOLVENTS.keys()) + ['IPA:Water Mixture','Other']
+materials_list = list(prop.MATERIALS.keys()) + ['Other']
 
-#water follows different trends
-def water_viscosity(temp):
-    """Returns viscosity of water using third order exponential"""
-    kelvin = temp+273.15
-    A = 2.3168E-12
-    B = 4729.66128
-    C = 0.04265
-    D = -2.14282E-5
-    centipoise = A*np.exp(B/kelvin+C*kelvin+D*kelvin**2)
-    return(centipoise / 1000)
-
-def water_density(temp):
-    """Returns the density of water using third order polynomial"""
-    C = 0.99997
-    B1 = 3.1087E-5
-    B2 = -6.45401E-6
-    B3 = 2.09654E-8
-    return((C+B1*temp+B2*temp**2+B3*temp**3)*1000)
-
-def speed_equivalence(t_1, w_1, n_1, t_2, n_2, p_NS1, p_NS2, p_l1, p_l2):
-    """Returns the angular rotation to match experiments with constant geometry"""
-    k = np.sqrt((p_NS1-p_l1)/(p_NS2-p_l2))
-    w2 = k * (t_1*n_2*w_1**2)/(t_2*n_1)
-    return np.sqrt(w2)
+##_Define key functions for calculations_##
 
 def supernatant_fraction_arrays(layer_number, lateral_size, time_hour, rpm, r1, r2):
     "Produces an array of fraction remaining in the supernatant from arrays of thickness and lateral size"
@@ -123,10 +69,6 @@ def mixed_property_function(w, T, c):
     return(c[0] + w*c[1] + T*c[2] + (w**2)*c[3] + w*T*c[4] + (T**2)*c[5] + (w**3)*c[6] + (w**2)*T*c[7] + w*(T**2)*c[8] + (T**3)*c[9])
 
 
-pS = 1595           #surfactant layer density, from Hirsham using anhydrous cholate on surface
-d0 = 4.25E-10       #surfactant layer thickness, from Hirsham
-
-
 ##_FRONT END_##
 st.title('Modelling Centrifugation of 2D Nanomaterials')
 st.subheader('CONFIDENTIAL')
@@ -148,26 +90,26 @@ if material == 'Other':
     pNS = st.number_input('Density of Custom Material in $g$ $cm^{-3}$', key='matdensity1', value=2.0)*1000
     d1 = st.number_input('Monolayer thickness from unit cell in Å', key='matthickness', value=3.0)*1E-10
 else:
-    pNS = MATERIALS[material][0]
-    d1 = MATERIALS[material][1]
-    k1 = MATERIALS[material][2]
-    k2 = MATERIALS[material][3]
+    pNS = prop.MATERIALS[material][0]
+    d1 = prop.MATERIALS[material][1]
+    k1 = prop.MATERIALS[material][2]
+    k2 = prop.MATERIALS[material][3]
 
 solvent = st.selectbox('Pick a solvent:',solvent_list)
 temp = st.slider('Temperature (use left/right arrow keys for fine adjustment', min_value=5, max_value=50, key='temperature1')
 if solvent == 'Water':
-    pL = water_density(temp)
-    n = water_viscosity(temp)
+    pL = prop.water_density(temp)
+    n = prop.water_viscosity(temp)
 elif solvent == 'Other':
     pL = st.number_input('Density of Custom Solvent in $g$ $cm^{-3}$', key='soldensity1')*1000
     n = st.number_input('Viscosity of Custom Solvent in $cP$', key='solvisc1')/1000
 elif solvent == 'IPA:Water Mixture':
     composition = st.slider('Weight Content of Alcohol', min_value=0.0, max_value=1.0, value=0.5, key='composition2')
-    pL = mixed_property_function(composition, temp, IPAWaterDensity)*1000
-    n = mixed_property_function(composition, temp, IPAWaterViscosity)/1000
+    pL = prop.mixed_property_function(composition, temp, IPAWaterDensity)*1000
+    n = prop.mixed_property_function(composition, temp, IPAWaterViscosity)/1000
 else:
-    pL = solvent_density(temp, solvent)
-    n = solvent_viscosity(temp, solvent)
+    pL = prop.solvent_density(temp, solvent)
+    n = prop.solvent_viscosity(temp, solvent)
 
 st.markdown('''*Illustrations of rotor geometry included in Theoretical Discussion*''')
 r1 = st.number_input('$R_1$ - Radius from axis to top of the liquid in cm', value=7.1)/100
